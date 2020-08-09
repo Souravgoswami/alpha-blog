fd51c27c84ef2de818ae45b3709f6098bfd43faf
Sourav Goswami <souravgoswami@protonmail.com>
2020-08-09 14:11:10 +0530

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
 	has_many :articles
+	EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :username, presence: true,
+		uniqueness: { case_sensitive: false },
+		length: { minimum: 3, maximum: 50 }
+
+	validates :email, presence: true,
+		uniqueness: { case_sensitive: false },
+		length: { maximum: 105 },
+		format: { with: EMAIL_REGEXP }
 end
