d78182bbbf0dbb89dc08ae643ab55cffe0ff6ed6
Sourav Goswami <souravgoswami@protonmail.com>
2020-08-09 13:43:39 +0530

    Added user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_174408) do
+ActiveRecord::Schema.define(version: 2020_08_09_075001) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_08_08_174408) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "author"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
