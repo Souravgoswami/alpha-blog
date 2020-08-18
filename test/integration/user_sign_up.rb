c57218073e915b2f638e348311b16cf7720cb644
Sourav Goswami <souravgoswami@protonmail.com>
2020-08-19 02:50:44 +0530

    Added basic integration test for user_sign_up (GUI)

--- test/integration/user_sign_up.rb
+++ test/integration/user_sign_up.rb
@@ -1,15 +1,36 @@
 require 'test_helper'
 
-class UserSignUp::ActionDispatch::IntegrationTest
+class UserSignUp < ActionDispatch::IntegrationTest
 	def setup
 		@user = User.create!(
-			username: 'admin'
+			username: 'admin',
 			email: 'admin@admin.admin',
-			password: 'adminuser',
+			password: 'adminuser'
 		)
 	end
 
-	test 'Users can be added by admin users' do
+	test 'Users can be created' do
+		u = nil
+		assert_difference('User.count', 1) do
+			u = User.create!(
+				username: 'example',
+				email: 'example@example.example',
+				password: 'exampleuser'
+			)
+		end
+	end
 
+	test 'Admin users can be created' do
+		u = nil
+		assert_difference('User.count', 1) do
+			u = User.create!(
+				username: 'example',
+				email: 'example@example.example',
+				password: 'exampleuser',
+				admin: true
+			)
+		end
+
+		assert u.admin?
 	end
 end
